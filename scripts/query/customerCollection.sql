a86f5264b72a25c8ccc6fe15b8447b166b6f3737
Juan Pineda <juancarlospinedafe073@gmail.com>
2023-07-05 17:32:26 -0500

    Modificacion de la entra de consulta para recibir varios parametros

--- scripts/query/customerCollection.sql
+++ scripts/query/customerCollection.sql
@@ -1,5 +1,5 @@
 SELECT T0."CardCode", T0."CardName", T0."LicTradNum", T0."AliasName", T0."Balance", TO_VARCHAR(T1."DocDueDate",'DD/MM/YYYY') AS "Fecha", T0."E_Mail"
 FROM {0}.OCRD T0
 INNER JOIN {0}.ORIN T1 ON T0."CardCode" = T1."CardCode"
-WHERE T0."CardCode" = 'CN1092016542'
+WHERE T0."CardCode" = '{1}'
 ORDER BY T1."DocDueDate"

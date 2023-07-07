f69983f33a5167d8d042d469be5e3e702ea7fcf6
Juan Pineda <juancarlospinedafe073@gmail.com>
2023-07-07 17:34:18 -0500

    Se modifica documentos relacionados al customerCollection (archivos Python y consulta SQL); se modifica la funcion formatNumberMoney (esta funcion presenta el error 'line 64, in formatNumberMoney num_separated = [a for a in number_str]) TypeError: 'int' object is not iterable)

--- scripts/generator/a.py
+++ scripts/generator/a.py
@@ -1,7 +1,23 @@
-import pandas as pd
+c = 1002000
+b = str(c)
+parameter = 0
 
-df = pd.read_excel('SendEmailAutomatic/scripts/docs/ClientesDeudores.xlsx', header=None)
-df_array = df.values
+coin = "${:,.2f}".format(c).replace(",","n").replace(".",",").replace("n",".")
 
-for j in range(len(df_array)):
-    print(df_array[j])
+num_separated = [a for a in b]
+
+if len(num_separated) <= 6:
+    ref = "pesos"
+else:
+    num_lim = num_separated[-6:]
+
+    for a in range(len(num_lim)):
+        parameter = parameter + int(num_lim[a])
+
+    if parameter == 0:
+        ref = "de pesos"
+    else:
+        ref = "pesos"
+
+print(coin)
+print(ref)

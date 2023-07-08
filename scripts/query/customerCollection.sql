6aa1e8cafed90b1adbcc5b7c4bbee80e8e8c89cb
Juan Pineda <juancarlospinedafe073@gmail.com>
2023-07-08 12:56:19 -0500

    Se cambio la estructura del SQL customerCollection (al usar el where en ciertos cadrcode, la consulta no arroja resultados aun sabiendo que existen)

--- scripts/query/customerCollection.sql
+++ scripts/query/customerCollection.sql
@@ -1,7 +1,23 @@
 SELECT T0."CardCode", T0."CardName", T0."LicTradNum", T0."AliasName", T0."Balance", TO_VARCHAR(T1."DocDueDate",'DD/MM/YYYY') AS "Fecha", 
-T0."E_Mail", T2."PymntGroup"
+T0."E_Mail", T0."GroupNum",
+CASE T0."GroupNum"
+WHEN -1 THEN -1
+WHEN 1 THEN 30
+WHEN 2 THEN 60
+WHEN 3 THEN 90
+WHEN 5 THEN 10
+WHEN 6 THEN 45
+WHEN 7 THEN 5
+WHEN 8 THEN 8
+WHEN 9 THEN 15
+WHEN 10 THEN 40
+WHEN 11 THEN 3
+WHEN 12 THEN 1
+WHEN 13 THEN 2
+WHEN 14 THEN 25
+ELSE -1
+END AS "Condicion de pago"
 FROM {0}.OCRD T0
 INNER JOIN {0}.ORIN T1 ON T0."CardCode" = T1."CardCode"
-INNER JOIN {0}.OCTG T2 ON T0."GroupNum" = T2."GroupNum"
-WHERE T0."CardCode" = {1}
-ORDER BY T1."DocDueDate"
+WHERE T0."CardCode" = {1} --El WHERE no funciona con este codigo
+ORDER BY T1."DocDueDate" 

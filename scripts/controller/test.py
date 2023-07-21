c719b61c0a717ed3fbfe4379e37bc6e693f5a210
Juan Pineda <juancarlospinedafe073@gmail.com>
2023-07-21 09:14:23 -0500

    IrShippingInventoryReport.py: Continua su desarrollo; menssage.html: Cambios esteticos

--- scripts/controller/test.py
+++ scripts/controller/test.py
@@ -2,10 +2,4 @@ from generator.parrettoGeneration import ParrettoSend
 from components.dataExtract import extracJSON
 
 def textExtraction():
-    dataJSON = extracJSON('supplier.json')
-    list_key = []
-
-    for key in dataJSON:
-        list_key.append(key)
-        
-    print(list_key)
+    print()
--- scripts/generator/IrInventoryReportGeneration.py
+++ scripts/generator/IrInventoryReportGeneration.py
@@ -13,19 +13,41 @@ class InventoryReportSend():
     Se encarga de generar el proceso de envio de la sabana de ventas para el proceso de 
     solicitudes de invercción 
     """
-    def __init__(self, schemeDB, ):
+    def __init__(self, schemeDB, codeStore, codeSupplier):
         self.schemeDB = schemeDB
+        self.codeStore = codeStore
+        self.codeSupplier = codeSupplier
         
     
-    def transformData(self, supplier):
-        text = ConverText.converTextFormatSQL('IrInventoryReport.sql', self.schemeDB)
+    def transformData(self):
+        text = ConverText.converTextFormatSQL('IrInventoryReport.sql', self.schemeDB, 
+                                              self.codeStore, self.codeSupplier)
         datas, nameRows = extracData(text)
-        ConverText.ConverDataXlsx(nameRows,datas,'Parretto de Ventas.xlsx')
+        ConverText.ConverDataXlsx(nameRows,datas,'Productos con mas de 60 dias en bodega.xlsx')
         print("Elemento Guardado.")
 
-    def processingData(self, dataJSON):
-        list_key = []
+    def processingData(self, dataJSON, way):
+        listKey = []
+        i = 0
+        storePlace = []
+
         for key in dataJSON:
-            list_key.append(key)
+            listKey.append(key)
+
+        if way == 0: 
+            DBKey = str(listKey[0]).replace("[","").replace("]","")
+            masterKey = [listKey[2], listKey[3]]
+        elif way == 1:
+            DBKey = str(listKey[1]).replace("[","").replace("]","")
+            masterKey = [listKey[4], listKey[5], listKey[6]]
+
+        schemeConformedElements = dataJSON[listKey[way]]
+
+        for title, data in schemeConformedElements.items():
+            if i < 4:
+                i += 1
+            if  i >= 4:
+                storePlace.append(data)
         
-        if key
+        return masterKey, DBKey, storePlace
+            

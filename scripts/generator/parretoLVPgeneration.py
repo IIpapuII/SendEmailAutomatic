810591d7fbf1a1e229ad0f9296b111f017971f1a
IsaacMoree <juancarlospinedafe073@gmail.com>
2023-12-18 14:32:29 -0500

    Testeo de Proyecto - Levapan

--- scripts/generator/parretoLVPgeneration.py
+++ scripts/generator/parretoLVPgeneration.py
@@ -18,18 +18,18 @@ class parretoLVPsend(Structure):
         day_int = date.today().day
         day_str = calendar.day_name[date.today().weekday()]
 
-        # if day_int == 1:        #Si es el primer dia del mes; arrojara cierre del mes anterior
-        #     currentMonth = date.today()                         #Toma la fecha actual
-        #     currentMonth.replace(day = 1)                       #Transforma al primer dia del mes actual
-        #     endDate = currentMonth - timedelta(days = 1)        #Transforma al ultimo dia del mes anterior
-        #     initDate = endDate.replace(day = 1)                 #Transforma al primer dia del mes anterior
+        if day_int == 1:        #Si es el primer dia del mes; arrojara cierre del mes anterior
+            currentMonth = date.today()                         #Toma la fecha actual
+            currentMonth.replace(day = 1)                       #Transforma al primer dia del mes actual
+            endDate = currentMonth - timedelta(days = 1)        #Transforma al ultimo dia del mes anterior
+            initDate = endDate.replace(day = 1)                 #Transforma al primer dia del mes anterior
 
-        #     endDate.strftime("%Y%m%d")             #Formatea fecha de fin
-        #     initDate.strftime("%Y%m%d")            #Formatea fecha de inicio
+            endDate.strftime("%Y%m%d")             #Formatea fecha de fin
+            initDate.strftime("%Y%m%d")            #Formatea fecha de inicio
         
-        # elif day_str == 'Friday':          #Si es viernes; arrojara lo que lleva del mes actual
-        endDate = date.today().strftime("%Y%m%d")                       #Entrega del ultima dia del mes actual formateado
-        initDate = date.today().replace(day = 1).strftime("%Y%m%d")     #Entrega del primer dia del mes actual formateado
+        elif day_str == 'Friday':          #Si es viernes; arrojara lo que lleva del mes actual
+            endDate = date.today().strftime("%Y%m%d")                       #Entrega del ultima dia del mes actual formateado
+            initDate = date.today().replace(day = 1).strftime("%Y%m%d")     #Entrega del primer dia del mes actual formateado
 
         text = ConverText.converTextFormatSQL('LVPparreto.sql', self.schemeDB, self.wareHouse, initDate, endDate, self.wareCellers)
         data , description = extracData(text)
